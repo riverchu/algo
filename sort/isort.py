325114c0d497835a5a995d611df4dcc59fb55280
indigo <zeroindigo@163.com>
2018-04-02 08:35:09 +0800

    unfinished

--- sort/isort.py
+++ sort/isort.py
@@ -10,12 +10,23 @@ class InsertSort(object):
     def isort(self):
         L = self.L
         for i in range(1,len(L)):
+            key = L[i]
+            for j in range(i,0,-1):
+                if L[j-1]>key:
+                    L[j] = L[j-1]
+                else:
+                    break
+            if L[j-1]>key:L[j-1] = key
+            print key,j-1
+            print L
+            '''
             j = i
             k = L[i]
             while j>0 and L[j-1]>k:
                 L[j] = L[j-1]
                 j-=1
                 L[j] = k
+                '''
 
     def show(self):
         print self.L
