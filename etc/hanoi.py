863d0398fb76bae3b47acf4c37e0e751b1859f16
indigo <humodi2017@outlook.com>
2018-04-04 17:35:16 +0800

    汉诺塔实现

---
+++ etc/hanoi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+def move(n,a,b,c):
+    if n is 1:
+        print(a,'-->',c)
+    else:
+        move(n-1,a,c,b)
+        move(1,a,b,c)
+        move(n-1,b,a,c)
+
+move(3,'A','B','C')
