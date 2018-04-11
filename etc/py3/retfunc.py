ab0d5c6cf10d137983b39dc92297ac306cb45087
indigo <humodi2017@outlook.com>
2018-04-11 16:33:57 +0800

    返回函数

---
+++ etc/py3/retfunc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#coding = utf-8
+
+def createCounter():
+    n = [0]
+    def counter():
+        n[0]+=1
+        return n[0]
+    return counter
+
+counterA = createCounter()
+print(counterA(), counterA(), counterA(), counterA(), counterA()) # 1 2 3 4 5
+counterB = createCounter()
+if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+    print('测试通过!')
+else:
+    print('测试失败!')
