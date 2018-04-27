f3651193c7e47b1c8aeda24fc4796dd6da15c19a
indigo <humodi2017@outlook.com>
2018-04-27 17:01:41 +0800

    修正

--- sort/qsort.py
+++ sort/qsort.py
@@ -12,19 +12,19 @@ class QuickSort(object):
         if(left >= right):
             return L
 
-        i = left
-        j = right
-        key = L[i]
+        l = left
+        r = right
+        key = L[l]
 
-        while i<j:
-            while i<j and key<=L[j]:j-=1
-            L[i] = L[j]
-            while i<j and L[i]<=key:i+=1
-            L[j] = L[i]
+        while l<r:
+            while l<r and key<=L[r]:r-=1
+            L[l] = L[r]
+            while l<r and L[l]<=key:l+=1
+            L[r] = L[l]
 
-        L[i] = key
-        self.qsort(left,i-1)
-        self.qsort(i+1,right)
+        L[l] = key
+        self.qsort(left,l-1)
+        self.qsort(l+1,right)
         return L
 
     def show(self):
